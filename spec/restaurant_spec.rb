064bc3696c6caff6b192b9b5030461450c4fb6b4
Blake Saylor <blakesaylor@gmail.com>
2022-05-11 10:21:48 -0600

    Update to fix test one of iteration four

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -73,8 +73,8 @@ RSpec.describe Restaurant do
       restaurant1 = Restaurant.new('6:00', 'Fuel Cafe')
       restaurant2 = Restaurant.new('16:00', 'Il Poggio')
 
-      expect(restaurant1.announce_closing_time).to eq "Fuel Cafe will be closing at 11:00AM"
-      expect(restaurant2.announce_closing_time).to eq "Il Poggio will be closing at 11:00PM"
+      expect(restaurant1.announce_closing_time(5)).to eq "Fuel Cafe will be closing at 11:00AM"
+      expect(restaurant2.announce_closing_time(7)).to eq "Il Poggio will be closing at 11:00PM"
     end
   end
 
