13c86fa18971ca8c9f341254c3e4074486f2e31b
Michael Koulouvaris <mikekoul@gmail.com>
2022-05-11 10:56:56 -0600

    Final commit

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,18 +1,32 @@
 class Restaurant
-  attr_reader :opening_time, :name, :dishes
+  attr_reader :opening_time, :name, :dishes, :menu
   def initialize(opening_time, name)
     @opening_time = opening_time
     @name = name
     @dishes = []
+    @menu = []
   end
 
   def add_dish(dish)
     @dishes << dish
   end
 
-  # def closing_time(hours_open)
-  #
+  def menu_dish_names
+    dishes.map(&:upcase)
+  end
+
+  # def open_for_lunch?
+  #   opening_time.chop.chop.chop.to_i
+  #   if opening_time <= 12
+  #     true
+  #   else
+  #     false
+  #   end
   # end
 
+  # def closing_time(hours_open)
+  #   opening_time.chop.chop.chop.to_i
+  #   opening_time += hours_open
+  # end
 
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -38,11 +38,37 @@ RSpec.describe Restaurant do
 
     it "can add dishes" do
       restaurant = Restaurant.new('16:00', 'Il Poggio')
+
       restaurant.add_dish('Burrata')
       restaurant.add_dish('Pizzetta')
       restaurant.add_dish('Ravioli')
 
       expect(restaurant.dishes).to eq(['Burrata', 'Pizzetta', 'Ravioli'])
+    end
+
+    xit "is restaurant1 open for lunch" do
+      restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
+
+      restaurant1.open_for_lunch?
+
+      expect(restaurant1.open_for_lunch?).to be true
+    end
+
+    xit "is restaurant2 open for lunch" do
+      restaurant2 = Restaurant.new('16:00', 'Il Poggio')
+
+      restaurant2.open_for_lunch?
+
+      expect(restaurant2.open_for_lunch?).to be false
+    end
+
+    it "menu items" do
+      restaurant2 = Restaurant.new('16:00', 'Il Poggio')
+
+      restaurant2.add_dish('Burrata')
+      restaurant2.add_dish("Pizzetta")
+      restaurant2.add_dish("Ravioli")
+      expect(restaurant2.menu_dish_names).to eq(["BURRATA", "PIZZETTA", "RAVIOLI"])
     end
 
   end
