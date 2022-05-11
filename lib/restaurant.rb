58430bfdfa8891eeae14a3275ec76db942c9c7c3
philmarcu <philmark18@gmail.com>
2022-05-11 09:21:34 -0600

    Finish Iteration 2

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -8,4 +8,14 @@ class Restaurant
     @dishes = []
   end
 
+  def closing_time(hour)
+    close = (@opening_time.to_i + hour)
+    close_hours = close.to_s
+    hour_format = close_hours.insert(2, ":00")
+  end
+
+  def add_dish(dish)
+    @dishes << dish
+  end
+
 end
