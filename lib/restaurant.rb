81e610aee0aa15ab3c4d8260a92a33763c397306
Bryan Shears <brshears@gmail.com>
2022-05-11 10:01:32 -0600

    Initialize Restaurant Name & Opening Time

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,11 @@
 class Restaurant
+  attr_accessor :name, :opening_time, :dishes, :closing_time
+  def initialize(opening_time, name)
+      @name = name
+      @opening_time = opening_time
+      @dishes = []
+  end
+
+
 
 end
