6e67dd387a15c43ec1b5866bc2f77abfa43dc379
Jared Hardinger <jaredhardinger@gmail.com>
2022-05-11 12:39:58 -0400

    Complete iteration 3

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -18,5 +18,16 @@ class Restaurant
 
   end
 
+  def open_for_lunch?
+    opening_time.to_i < 12
+  end
+
+  def menu_dish_names
+    dishes.map do |dish|
+      dish.upcase
+    end
+
+  end
+
 
 end
