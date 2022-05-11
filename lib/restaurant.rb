d76386eeba65a1f815d8f71303b2a07ac6f2b49e
Alex M <Alexm717@hotmail.com>
2022-05-11 10:00:57 -0700

    initial commit

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,13 @@
 class Restaurant
-
-end
+  attr_reader :opening_time,
+              :name
+              :dishes
+  def initialize(opening_time,name)
+    @opening_time="10:00"
+    @name= "Fuel Cafe"
+    @dishes=[]
+    end
+    def closing_time(closing)
+      @closing_time << "16:00"
+    end
+  end
