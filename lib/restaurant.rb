693885a9481327b71a80662a8f89cc660cb8aa73
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-05-11 11:03:33 -0600

    commit changes

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,24 @@
 class Restaurant
+attr_reader :opening_time, :name, :dishes, :closing_time
 
+  def initialize(opening_time, name)
+    @opening_time = opening_time
+    @name = name
+    @dishes = [] #starts with empty array
+    @closing_time = ""
+
+  end
+
+  def closing_time(end_time)
+    if 8
+      then end_time = "18:00"
+    end
+    if 7
+      then end_time = "23:00"
+    end
+  end
+
+  def add_dish(flavor)
+    @dishes << flavor #pushes new flavor dish to array
+  end
 end
