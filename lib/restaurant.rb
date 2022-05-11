2e48e314773c599ad763cc651f2850a3138bdd0c
Ben Silverstein <benjsilverstein@comcast.net>
2022-05-11 09:39:55 -0600

    Iteration 1 define methods

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,14 @@
 class Restaurant
+  attr_reader :opening_time, :name, :dishes
+
+  def initialize(opening_time, name)
+    @opening_time = opening_time
+    @name = name
+    @dishes = []
+  end
+
+  def add_dish(dishes)
+    @dishes << dishes
+  end
 
 end
