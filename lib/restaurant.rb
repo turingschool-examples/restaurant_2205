4561611364e6c9861e26583deca1675b94516449
Lucas Colwell <colwellc37@gmail.com>
2022-05-11 10:04:31 -0600

    Feat: create closing time method

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Restaurant
   attr_reader :opening_time,
               :name,
@@ -9,6 +11,13 @@ class Restaurant
     @dishes = []
   end
 
+  def closing_time(hours)
+    # require 'pry'; binding.pry
+    close = @opening_time.to_f + hours.to_f
+    close_string = '%.2f' % close
+    close_string.gsub(".", ":")
+  end
 
+  
 
 end
