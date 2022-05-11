68f640a032cba0a20361db0db5e7eaf0852bd09c
Riley McCullough <rileybmccullough@gmail.com>
2022-05-11 14:45:13 -0600

    initial commit

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,17 @@
 class Restaurant
 
+  attr_reader :opening_time, :name, :dishes
+  def initialize(opening_time, name)
+    @opening_time = opening_time
+    @name = name
+    @dishes = []
+
+    @opening_time_integer = @opening_time.to_i
+    def closing_time(hours_open)
+
+      closing_time = (@opening_time_integer + hours_open).to_s + ":00"
+
+
+    end
+    end
 end
