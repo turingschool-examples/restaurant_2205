0b9a68dfe6899f00da39b11cb0374ba2bd493b9c
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-11 11:37:17 -0400

    Add menu_dish_names method

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -17,4 +17,24 @@ class Restaurant
     @dishes << dish
   end
 
+  def open_for_lunch?
+    open_time_hour = @opening_time[0,2].to_i
+    if open_time_hour < 12
+      return true
+    else
+      return false
+    end
+  end
+
+  def menu_dish_names
+
+    dishes_aggregator = []
+    dishes.each do |dish|
+      dishes_aggregator << dish.upcase
+    end
+    
+    return dishes_aggregator
+
+  end
+
 end
