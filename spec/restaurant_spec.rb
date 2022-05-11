391387988633ee327172bc8fcbe94fb945491a6b
Thiago Silveira <thiago1992@yahoo.com>
2022-05-11 09:40:06 -0600

    Test: Restaurant can announce a closing time

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -67,4 +67,13 @@ RSpec.describe Restaurant do
     end
 
   end
+
+  describe "Iteration 4" do
+    it 'can announce closing time' do
+      restaurant1 = Restaurant.new('6:00', 'Fuel Cafe')
+      restaurant2 = Restaurant.new('16:00', 'Il Posto')
+      expect(restaurant1.announce_closing_time(5)).to eq("Fuel Cafe will be closing at 11:00AM")
+      expect(restaurant2.announce_closing_time(7)).to eq("Il Posto will be closing at 11:00PM")
+    end
+  end
 end
