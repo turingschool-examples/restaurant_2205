2d01c946858f1235494e9e6b068f41733e8ca8a8
Chris Lyman <christopher.lyman0@gmail.com>
2022-05-11 12:23:39 -0400

    Complete through iteration 3

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,31 @@
 class Restaurant
+attr_accessor :opening_time, :name, :dishes
 
+  def initialize(opening_time, name)
+    @opening_time = opening_time
+    @name = name
+    @dishes = []
+  end
+
+  def closing_time(closing_time)
+    actual_closing_time = opening_time.to_i + closing_time
+    "#{actual_closing_time}:00"
+  end
+
+  def add_dish(dish)
+    dishes << dish
+  end
+
+  def open_for_lunch?
+    if opening_time.to_i < 13
+#require "pry"; binding.pry
+      true
+    else
+      false
+    end
+  end
+
+  def menu_dish_names
+    dishes.map!(&:upcase)
+  end
 end
