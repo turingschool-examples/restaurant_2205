a0671c33effd92ae98a727d73ed0a4f6d9ff96d6
Kevin Ta <kvnta@yahoo.com>
2022-05-11 10:59:16 -0600

    Finish ics

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,18 @@
 class Restaurant
+  attr_reader :opening_time, :name
+  def initialize (opening_time, name)
+    @opening_time = opening_time
+    @name = name
 
+  end
+  def dishes
+    dishes = []
+  end
+
+  def closing_time
+    @closing_time
+  end
+  def add_dish
+
+  end
 end
