b7e5c72b799a360838ef666cf2eadf13823e9553
philmarcu <philmark18@gmail.com>
2022-05-11 10:34:34 -0600

    Final attempt at Iteration 4

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -30,4 +30,27 @@ class Restaurant
     @dishes.map {|dish| dish.upcase}
   end
 
+  def announce_closing_time(hour)
+    announce_time = (opening_time.to_i)
+    close_time = (announce_time + hour)
+    change_to_hr = if close_time > 12
+      close_time - 12
+    end
+    hour_format = change_to_hr.to_s.insert(2, ":00")
+    am_pm = if announce_time > 12
+      "PM"
+    else
+      "AM"
+    end
+    return "#{@name} will be closing at #{hour_format}#{am_pm}"
+  end
 end
+
+# Can't figure out the conditions to make restaurant1 time pass"
+
+
+# require 'date'
+# close_time = opening_time.to_i + hour
+# nu_time = close_time.to_s.insert(2, "")
+# # require "pry"; binding.pry
+# DateTime.parse("#{nu_time}").strftime("%M:%H")
