74cccf262b5d10e36d7b19c58d80d6afc5c853ec
Eli Sachs <easachs13@gmail.com>
2022-05-11 10:12:29 -0600

    Complete Restaurant class (iteration 4)

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -29,4 +29,18 @@ class Restaurant
     end
     menu_dish_names
   end
+
+
+  def announce_closing_time(hrs)
+    closing_time_to_12_hrs = @opening_time.to_i + hrs
+    if closing_time_to_12_hrs > 12
+      closing_time_to_12_hrs -= 12
+    end
+
+    closing_time_to_12_hrs_w_00 = closing_time_to_12_hrs.to_s.concat(':00')
+
+    (@opening_time.to_i + hrs) > 12 ? am_pm = "PM" : am_pm = "AM"
+
+    "#{name} will be closing at #{closing_time_to_12_hrs_w_00}#{am_pm}"
+  end
 end
