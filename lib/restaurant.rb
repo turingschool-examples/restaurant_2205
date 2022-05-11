ed03339b3902a86be32d3fc65e6b49c4c554d38d
Ben Silverstein <benjsilverstein@comcast.net>
2022-05-11 10:05:48 -0600

    Iteration 2 define methods

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,14 +1,21 @@
 class Restaurant
-  attr_reader :opening_time, :name, :dishes
+  attr_reader :opening_time, :name, :dishes, :run_time
 
   def initialize(opening_time, name)
     @opening_time = opening_time
     @name = name
     @dishes = []
+    @run_time = run_time
   end
 
   def add_dish(dishes)
     @dishes << dishes
+  end
+
+  def closing_time(run_time)
+    integer_opening_time = opening_time.to_i
+    integer_opening_time.to_i + run_time
+    "#{integer_opening_time + run_time}:00"
   end
 
 end
