7ae7299e524817f52e49f8bc14cbfaf1982aae7b
Nicole Esquer <nicole.esquer5@gmail.com>
2022-05-11 08:39:03 -0700

    Finished iteration 1

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,26 @@
 class Restaurant
+attr_accessor :opening_time, :name, :dishes
+  def initialize(opening_time, name)
+    @opening_time = opening_time
+    @name = name
+    @dishes = []
+
+  end
+
+
+  def opening_time
+  opening_time = "10:00"
+  end
+
+  def name
+    name = "Fuel Cafe"
+  end
+
+  def dishes
+    dishes = []
+  end
+
+
+
 
 end
