6f5b1c49b51cdbe0b67c06369c3134e042ab1e8c
Eli Sachs <easachs13@gmail.com>
2022-05-11 09:28:28 -0600

    Refactor Restaurant class (iteration 3)

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -18,4 +18,15 @@ class Restaurant
   def add_dish(dish)
     @dishes << dish
   end
+
+  def open_for_lunch?
+    @opening_time.to_i < 12
+  end
+
+  def menu_dish_names
+    menu_dish_names = @dishes.map do |dish|
+      dish.upcase
+    end
+    menu_dish_names
+  end
 end
