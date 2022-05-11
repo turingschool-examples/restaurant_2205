4c56aa848be5c9e6bdaa973472a9cc98dc331fc6
Eli Sachs <easachs13@gmail.com>
2022-05-11 09:23:51 -0600

    Initial commit

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,21 @@
 class Restaurant
 
+  attr_reader :opening_time,
+              :name,
+              :dishes
+
+  def initialize(opening_time, name)
+    @opening_time = opening_time
+    @name = name
+    @dishes = []
+  end
+
+  def closing_time(hrs)
+    closing_time = @opening_time.to_i + hrs
+    closing_time.to_s.concat(':00')
+  end
+
+  def add_dish(dish)
+    @dishes << dish
+  end
 end
