c265fec27a089aab0775f392465abd707f2f4d2e
Gauri Joshi <gauri728@gmail.com>
2022-05-11 08:56:40 -0700

    Add dishes test for Iteration 2

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -6,4 +6,14 @@ class Restaurant
     @dishes = []
   end
 
+  def closing_time(hours)
+    new_time = opening_time.to_i + hours
+
+  end
+
+  def add_dish(dish)
+    @dishes << dish
+  end
+
+  # sum = closing_time("10:00", 6, @name)
 end
