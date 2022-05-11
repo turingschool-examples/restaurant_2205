acc8fc3b77b271c6747ef3642245a9547c6fbc87
Anna Marie Sterling <sterling.am@gmail.com>
2022-05-11 09:55:57 -0600

    Finished Iteration 2

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,10 +1,17 @@
 class Restaurant
 attr_reader :opening_time, :name, :dishes
   def initialize(opening_time, name)
-    @opening_time = "10:00"
+    @opening_time = opening_time
     @name = name
     @dishes = []
   end
 
+  def closing_time(operating_hours)
+    integer_opening_time = opening_time.to_i
+    "#{integer_opening_time + operating_hours}:00"
+  end
 
+  def add_dish(menu_item)
+    @dishes << menu_item
+  end
 end
